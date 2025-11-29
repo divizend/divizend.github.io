98ce089f956456ae2f7a45e8bf68ca8ec04e893c
Julian Nalenz <julian.nalenz@divizend.com>
2025-11-29 15:39:02 +0100

    Fix all YAML indentation issues

--- setup.sh
+++ setup.sh
@@ -313,7 +313,7 @@ cat <<EOF > /etc/bento/streams/ingest_email.yaml
         # In a strict production environment, you would verify the svix-signature here.
         # Passing raw payload to stream for durability.
         - mapping: root = this
-    
+
     output:
       resource: s2_inbox_writer
 EOF
@@ -336,33 +336,33 @@ cat <<EOF > /etc/bento/streams/process_reverser.yaml
         let sender_domain = "${BASE_DOMAIN}"
         let sender_email = "reverser@" + \$sender_domain
 
-            # Business Logic: Reverse the text
-            # Splitting by empty string creates array of chars, reverse array, join back
-            let reversed_text = \$original_text.split("").reverse().join("")
+        # Business Logic: Reverse the text
+        # Splitting by empty string creates array of chars, reverse array, join back
+        let reversed_text = \$original_text.split("").reverse().join("")
 
         # Construct Resend API Payload with automatically determined emails
         root.from = "Reverser <" + \$sender_email + ">"
         root.to = [\$receiver]
-            root.subject = "Re: " + \$subject
-            root.html = "<p>Here is your reversed text:</p><blockquote>" + \$reversed_text + "</blockquote>"
+        root.subject = "Re: " + \$subject
+        root.html = "<p>Here is your reversed text:</p><blockquote>" + \$reversed_text + "</blockquote>"
 
-    output:
-      resource: s2_outbox_writer
+output:
+  resource: s2_outbox_writer
 EOF
 
 # Stream 3: Egress - S2 Outbox -> Resend API
 cat <<EOF > /etc/bento/streams/send_email.yaml
-    input:
-      resource: s2_outbox_reader
+input:
+  resource: s2_outbox_reader
       
-    output:
-      http_client:
-        url: https://api.resend.com/emails
-        verb: POST
-        headers:
+output:
+  http_client:
+    url: https://api.resend.com/emails
+    verb: POST
+    headers:
       Authorization: "Bearer ${RESEND_API_KEY}"
-          Content-Type: "application/json"
-        retries: 3
+      Content-Type: "application/json"
+    retries: 3
         # If Resend fails, message stays in S2 (due to ack logic) or DLQ can be configured
 EOF
 
